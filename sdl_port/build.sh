a53d74a2855d9cffeba56813f7093f7dd7cb3b75
Lukas F. Hartmann <lukas@mntre.com>
2020-09-27 14:47:20 +0200

    sdl/build.sh: name executable 'soeki'

--- sdl_port/build.sh
+++ sdl_port/build.sh
@@ -1,2 +1,2 @@
-g++ -g networ.cpp independent/engine.cpp platform/sdl/assd.cpp independent/gfx32.cpp independent/zones.cpp independent/processing.cpp platform/sdl/textures.cpp platform/sdl/storage.cpp platform/sdl/network.cpp platform/sdl/master.cpp goutextri2.cpp -l SDL2 -l SDL2_image
+g++ -o soeki -g networ.cpp independent/engine.cpp platform/sdl/assd.cpp independent/gfx32.cpp independent/zones.cpp independent/processing.cpp platform/sdl/textures.cpp platform/sdl/storage.cpp platform/sdl/network.cpp platform/sdl/master.cpp goutextri2.cpp -l SDL2 -l SDL2_image
 
